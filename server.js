134f13bf1e738cd01b5617b59b804bf4ecbdbd83
David Slusser <dslusser13@gmail.com>
2022-05-25 07:49:12 -0700

    added whitelist

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELI
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
-  originWhitelist: ['https://dslusser.com,http://127.0.0.1:5500'],
+  originWhitelist: ['https://dslusser.com,https://niagarawater--tst1.custhelp.com'],
   requireHeader: ['origin', 'x-requested-with'],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
